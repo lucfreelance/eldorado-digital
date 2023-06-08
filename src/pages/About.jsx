8868766aafffcb76f3bef96bcafb840a621bd251
sslluuccaa <soelucas3@gmail.com>
2023-06-08 02:45:24 -0500

    bis

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 // import React from 'react';
 import Sparky from '../img/sparky.jpg'
+import './styles.css'
 
 const About = () => {
   return (
     <div>
       <img src={Sparky} alt="Sparky moment" className="about-image" />
-      <h2>About Eldorado Book Store - Be our guess 🏠</h2>
+      <h1>About Eldorado Book Store</h1>
+      <h2>Be our guess 🏠</h2>
       <p>Welcome to Eldorado, where the world of low-content books comes to life! Our mission is to inspire, educate, and entertain through a carefully curated collection of engaging puzzles, word search games, Sudoku challenges, crosswords, and coloring books suitable for all ages.</p>
 <p>Besides, you can also dive into our virtual wonderland playground of activities and explore a vast selection of minigames, some of then designed on our own.</p>
 <p>Choose from a variety of formats - physical books, e-books, notebooks - to suit your preference and enjoy these captivating activities anytime, anywhere.</p>
